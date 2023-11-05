9fe540e94bdb2aa6fc6b4c39b156ff1b3a6b662e
gusta <gusta@DESKTOP-ANHOHH9>
2023-11-05 01:26:31 -0300

    add query curta

--- index.js
+++ index.js
@@ -80,8 +80,8 @@ function draw() {
     },
 
     initialCypher:
-      "MATCH (n)-[r]-(m) RETURN n, r, m"
-
+     // "MATCH (n)-[r]-(m) RETURN n, r, m"
+     "MATCH (n:BNCC_UnidadeDeEstudo)-[r1:POSSUI]->(e:BNCC_Habilidade) RETURN n AS node, r1 AS relationship, e AS target UNION MATCH (n:BNCC_UnidadeDeEstudo)<-[r2:PERTENCE]-(e:BNCC_Habilidade) RETURN n AS node, r2 AS relationship, e AS target"
     // "MATCH p = (:BNCC_AreaDeConhecimento)-[:PERTENCE]-> () RETURN p;"
 
     //MATCH (a:SBC_CompetenciaGeralEgresso) RETURN a as Node, "CompetenciaGeralEgresso" as Type UNION MATCH (b:SBC_Conteudo) RETURN b as Node, "Conteudo" as Type UNION MATCH (c:SBC_Eixo) RETURN c as Node, "Eixo" as Type UNION MATCH (d:SBC_CompetenciaEspecificaEgresso) RETURN d as Node, "CompetenciaEspecificaEgresso" as Type UNION MATCH (e:SBC_CompetenciaDerivada) RETURN e as Node, "CompetenciaDerivada" as Type;
